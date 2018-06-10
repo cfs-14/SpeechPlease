1b82cc1e932bdc2f33186d913bb9d728ed002385
cfs-14 <cfsandoval14@gmail.com>
2018-06-09 17:09:24 -0700

    Completed PhrasesCategoryActivity activity that will host the fragment that displays the categories.

--- app/src/main/java/com/example/gabav/speechplease/PhrasesCategoryActivity.java
+++ app/src/main/java/com/example/gabav/speechplease/PhrasesCategoryActivity.java
@@ -1,10 +1,46 @@
 package com.example.gabav.speechplease;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by gabav on 09-Jun-18.
  */
 
-public class PhrasesCategoryActivity extends AppCompatActivity {
+public class PhrasesCategoryActivity extends AppCompatActivity
+{
+    //In this activity we host the fragment recyclerView
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //remove actionBar.
+        try
+        {
+            getSupportActionBar().hide();
+        }
+        catch(NullPointerException n)
+        {
+            //(H);
+        }
+
+        setContentView(R.layout.category_words_fragment);
+
+        //set up the fragmentManager.
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_containero);
+
+        if(fragment == null)
+        {
+            fragment = new PhrasesCategoryFragment();
+
+            fragmentManager.beginTransaction().add(R.id.fragment_containero, fragment)
+                    .commit();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/gabav/speechplease/PhrasesCategoryFragment.java
@@ -0,0 +1,10 @@
+package com.example.gabav.speechplease;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by gabav on 09-Jun-18.
+ */
+
+public class PhrasesCategoryFragment extends Fragment {
+}
